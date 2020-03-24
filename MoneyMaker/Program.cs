884a73aee76e5bcff83b75a045e19c492ebe791c
ignacio <nachomattos78@gmail.com>
2020-03-24 19:52:29 +0000

    C# program

---
+++ MoneyMaker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Money Maker
+
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("How much do you want to convert?: ");
+            string amount = Console.ReadLine();
+            double amountInt = Convert.ToDouble(amount);
+            Console.WriteLine($"{amountInt} cents is equal to ..");
+
+            //Coins values
+            int gold = 10;
+            int silver = 5;
+
+            //number of gold coins “fit” into the amount
+            double goldCoins = Math.Floor(amountInt / gold);
+            double remaining = amountInt % gold;
+
+            //number of silver coins that “fit” into the remainder
+            double silverCoins = Math.Floor(remaining / silver);
+            double silverRemain = remaining % silver;
+
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            // number of bronze coins 
+            Console.WriteLine($"Bronze coins: {silverRemain}");
+
+        }
+    }
+}
